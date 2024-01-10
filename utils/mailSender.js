984c79a1ca5398d06472ca85f738f4ed7f4888c7
HarshKatore <harsh.katore21@pccoepune.org>
2024-01-11 01:05:01 +0530

    email template  commit

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,34 +1,29 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-
 //this is just the mechanism of OPT sending not pre post its in main fn
-const mailSender = async(email,title,body) => {
-    try{
+const mailSender = async (email, title, body) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER, //mailt from which we send mailto user
+        pass: process.env.MAIL_PASS,
+      },
+    });
 
+    let info = await transporter.sendMail({
+      from: `XYZ`,
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
 
-        const transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            auth: {
-               user: process.env.MAIL_USER, //mailt from which we send mailto user
-               pass: process.env.MAIL_PASS,
-           },
-        });
+    console.log(info);
+    return info;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-        let info = await transporter.sendMail({
-            from:`XYZ`,
-             to: `${email}`,
-            subject: "${title}",
-            html: `${body}`
-         });
-
-         console.log(info);
-         return info;
-
-    }
-    catch(error){
-        console.log(error.message);
-    }
-}
- 
 module.exports = mailSender;
