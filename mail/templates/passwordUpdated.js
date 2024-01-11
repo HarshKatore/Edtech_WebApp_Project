2c387530e6336ed264d988123cea12c8cf891aec
HarshKatore <harsh.katore21@pccoepune.org>
2024-01-11 13:10:54 +0530

    logo change  commit

--- mail/templates/passwordUpdated.js
+++ mail/templates/passwordUpdated.js
@@ -55,8 +55,8 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+            <a href="https://edtech-web-app.vercel.app/"><img class="logo"
+                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="CoviLearn Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
@@ -66,7 +66,7 @@ exports.passwordUpdated = (email, name) => {
             </div>
             <div class="support">If you have any questions or need further assistance, please feel free to reach out to us
                 at
-                <a href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!
+                <a href="mailto:info@covilearn.com">info@covilearn.com</a>. We are here to help!
             </div>
         </div>
     </body>
